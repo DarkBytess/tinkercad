9167cb44af21f23695155b69c12b973ff92565ef
law <152932607+DarkBytess@users.noreply.github.com>
2024-03-04 17:26:50 -0300

    first commit

--- ex1.ino
+++ ex1.ino
@@ -1,7 +1,5 @@
 int led = 4;
 int bot = 6;
-int time;
-bool m = 0;
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -10,27 +8,10 @@ void setup() {
 }
 
 void loop() {
-    if (m ==0) {
-        time = 0;
-        for (int i = 0; i < 10; i++) {
-            digitalWrite(led, HIGH);
-            delay(1000 - time);
-            digitalWrite(led, LOW);
-            delay(1000 - time);
-            time += 50;
-        }
-        m = 1;
-    }
-    
-    if (m ==1) {
-        time = 0;
-        for (int i = 0; i < 10; i++) {
-            digitalWrite(led, HIGH);
-            delay(500 + time);
-            digitalWrite(led, LOW);
-            delay(500 + time);
-            time += 50;
-        }
-        m = 0;
-    }
+  
+  if (digitalRead(bot) == 1) {
+    digitalWrite(led, HIGH); 
+  }
+  
+  digitalWrite(led, LOW);
 }
